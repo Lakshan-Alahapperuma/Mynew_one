f9d6e609298e3577d0c3b35d7489847c797a5514
Lakshan Alahapperuma <147945056+cst22035@users.noreply.github.com>
2024-06-04 22:04:46 +0530

    FIRST STEP

---
+++ new5.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class new5{
+
+    public static void main(String args[]){
+
+        System.out.print("Enter your First Name : ");
+        Scanner s1 = new Scanner(System.in);
+
+        System.out.println(s1.nextLine());
+
+        System.out.print("Enter Your Second Name : ");
+        Scanner s2 = new Scanner(System.in);
+
+        System.out.println(s2.nextLine());
+
+      
+    }
+}
